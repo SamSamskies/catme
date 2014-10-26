54d8fd1a21d563d5bd30ac2f9bac15edc35c6b4b
Steven Miyakawa SAM <samprofessional@gmail.com>
2014-10-26 13:25:33 -0700

    MVP

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var Yo = require('yo-api');
+var yo = new Yo(process.env.CATME_YO_API_KEY);
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/yo', function(req, res) {
+app.get('/yo', function(req, res, next) {
+  yo.yo_link(req.query.username, 'http://thecatapi.com/api/images/get');
+  res.send();
 });
 
 app.listen(app.get('port'), function() {
